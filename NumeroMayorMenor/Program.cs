7d7574bd3a6139493be0ff613a8645cc5f1584dd
Israel Calderón de la Cruz <icalderondev@outlook.com>
2020-03-31 04:39:25 -0600

    Add project files.

---
+++ NumeroMayorMenor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroMayorMenor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A;
+            string B;
+
+            //while: Significa mientras
+            //Do While: Significa Hacer mientras
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Instroduzca dos valores distintos");
+                Console.WriteLine("Digite la variable A");
+                A = Console.ReadLine();
+                Console.WriteLine("Digite la variable B");
+                B = Console.ReadLine();
+            } while (A == B);
+
+            var letraA = Convert.ToInt32(A);
+            var letraB = Convert.ToInt32(B);
+
+            if (letraA > letraB)
+                Console.WriteLine("A es el mayor");
+            else
+                Console.WriteLine("B es el mayor");
+
+            Console.ReadKey();
+        }
+    }
+}
